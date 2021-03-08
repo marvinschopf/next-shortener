64617dc4af1e3a8c76968dd3d8fa7b92ee15f818
Marvin Schopf <marvin@schopf.biz>
2021-03-08 20:00:55 +0100

    UX improvements

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,18 +16,67 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { ChangeEvent, FunctionComponent, useState } from "react";
+import { NextPage } from "next";
+import { useState } from "react";
 import Layout from "../components/Layout";
+
+import { getBaseURL } from "./../helpers/meta";
+
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const Index: FunctionComponent = () => {
+import { FaClipboard, FaClipboardCheck } from "react-icons/fa";
+
+type Props = {
+	baseUrl: string;
+};
+
+const Index: NextPage<Props> = ({ baseUrl }) => {
 	const [targetUrl, setTargetUrl] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
+	const [createdShortlink, setCreatedShortlink] = useState(null);
+	const [shortLinkCopied, setShortLinkCopied] = useState(false);
 
 	return (
 		<Layout title="Next Shortener">
+			{createdShortlink && (
+				<Alert variant="success">
+					<p>
+						<b>Successfully created!</b>
+					</p>
+					<Form.Group>
+						<Form.Label>Shortlink</Form.Label>
+						<Row>
+							<Col xs={12} lg={11} md={8}>
+								<Form.Control
+									disabled
+									value={`${baseUrl}${createdShortlink.slug}`}
+								/>
+							</Col>
+							<Col lg={1} md={4} xs={12}>
+								<CopyToClipboard
+									text={`${baseUrl}${createdShortlink.slug}`}
+									onCopy={() => {
+										setShortLinkCopied(true);
+									}}
+								>
+									<Button variant="secondary" block>
+										{!shortLinkCopied && <FaClipboard />}
+										{shortLinkCopied && (
+											<FaClipboardCheck />
+										)}
+									</Button>
+								</CopyToClipboard>
+							</Col>
+						</Row>
+					</Form.Group>
+				</Alert>
+			)}
 			<Form
 				onSubmit={async (event) => {
 					event.preventDefault();
@@ -38,9 +87,10 @@ const Index: FunctionComponent = () => {
 							target: targetUrl,
 						}),
 					});
+					setIsLoading(false);
 					if (response.status === 200) {
 						const json = await response.json();
-						alert(json.shortlink.slug);
+						setCreatedShortlink(json.shortlink);
 					} else {
 						alert(response.status);
 					}
@@ -63,6 +113,10 @@ const Index: FunctionComponent = () => {
 			</Form>
 		</Layout>
 	);
+};
+
+Index.getInitialProps = async () => {
+	return { baseUrl: getBaseURL() };
 };
 
 export default Index;
