1d37b6166ef5571ae52122229c153fd73f6c32a4
Marvin Schopf <marvin@schopf.biz>
2021-03-09 14:03:44 +0100

    Fix module import

--- pages/_app.js
+++ pages/_app.js
@@ -19,10 +19,10 @@
 import "../styles/globals.css";
 import { appWithTranslation } from "next-i18next";
 
-import i18NextConfig from "./../next-i18next.config";
+import { i18n } from "./../next-i18next.config";
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
 }
 
-export default appWithTranslation(MyApp, i18NextConfig);
+export default appWithTranslation(MyApp, i18n);
